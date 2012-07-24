33ea5bc56cf245c11aeb52090c0f4ce890bb6c03
Kevin Munc <muncman@gmail.com>
2012-07-24 13:25:35 -0400

    [minor] Alternate way to hack around a limitation.

--- SOLIDTests/MUConceptTest.m
+++ SOLIDTests/MUConceptTest.m
@@ -29,3 +29,16 @@
 }
 
 @end
+
+// For helping avoid CLI issues.
+//#include <stdio.h>
+//// Prototype declarations
+//FILE *fopen$UNIX2003( const char *filename, const char *mode );
+//size_t fwrite$UNIX2003( const void *a, size_t b, size_t c, FILE *d );
+//
+//FILE *fopen$UNIX2003( const char *filename, const char *mode ) {
+//    return fopen(filename, mode);
+//}
+//size_t fwrite$UNIX2003( const void *a, size_t b, size_t c, FILE *d ) {
+//    return fwrite(a, b, c, d);
+//}
