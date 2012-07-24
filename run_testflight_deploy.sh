37f55d8afc8d0db0bb2ecf1d403fc3b8ce97554e
Kevin Munc <muncman@gmail.com>
2012-07-23 21:28:29 -0400

    Finally. It is working. Removed debugging output from script.

--- run_testflight_deploy.sh
+++ run_testflight_deploy.sh
@@ -1,6 +1,3 @@
-echo "You must export TF_TOKEN and TF_TEAM_TOKEN in Jenkins' 'Configure System' | 'Global properties' section."
-env
-echo TF_TOKEN = $TF_TOKEN
-echo TF_TEAM_TOKEN = $TF_TEAM_TOKEN
+echo "You must setenv/export TF_TOKEN and TF_TEAM_TOKEN for this script. Maybe set in Jenkins' 'Configure System' | 'Global properties' section."
 curl http://testflightapp.com/api/builds.json -F file=@build/Release-iphoneos/SOLID-Release-1.0.ipa -F dsym=@build/Release-iphoneos/SOLID-Release-1.0-dSYM.zip -F api_token=$TF_TOKEN -F team_token=$TF_TEAM_TOKEN -F notes="This is an autodeploy build from Jenkins via curl" -F notify=True 
 # -F distribution_lists=’<name of test distribution list>’ -F replace=False
