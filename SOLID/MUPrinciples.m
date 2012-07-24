0ef8f891d78d9f4f79f21154be36c1e6eda8ffe1
Kevin Munc <muncman@gmail.com>
2012-07-23 20:24:34 -0400

    TYPOS!

--- SOLID/MUPrinciples.m
+++ SOLID/MUPrinciples.m
@@ -39,7 +39,7 @@
         MUConcept *liskov = [[MUConcept alloc] 
                              initWithInitial:@"L"            
                              acronym:@"LSP" 
-                             name:NSLocalizedString(@"Liskov Substitution Princple", nil) 
+                             name:NSLocalizedString(@"Liskov Substitution Principle", nil) 
                              description:NSLocalizedString(@"Objects in a program should be replaceable with instances of their subtypes "
                                                            "without altering the correctness of that program. "
                                                            "Derived classes must be substitutable for their base classes.",       
@@ -47,7 +47,7 @@
         MUConcept *interface = [[MUConcept alloc] 
                                 initWithInitial:@"I" 
                                 acronym:@"ISP" 
-                                name:NSLocalizedString(@"Interface Segregation Princple", nil) 
+                                name:NSLocalizedString(@"Interface Segregation Principle", nil) 
                                 description:NSLocalizedString(@"Many client specific interfaces are better than one general purpose interface. "
                                                               "Make fine grained interfaces that are client specific.",         
                                                               @"ISP description.")];
