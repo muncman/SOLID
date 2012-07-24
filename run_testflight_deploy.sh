5e6aacf3fb91b3b64d329c02e2f2ae89e730e972
Kevin Munc <muncman@gmail.com>
2012-07-23 21:21:57 -0400

    Gah. Forgot the dollar sign to get the API token values I was trying to use.

--- run_testflight_deploy.sh
+++ run_testflight_deploy.sh
@@ -1,3 +1,3 @@
-echo "You must export TF_TOKEN and TF_TEAM_TOKEN in the Execute Shell block before calling this script."
-curl http://testflightapp.com/api/builds.json -F file=@build/Release-iphoneos/SOLID-Release-1.0.ipa -F dsym=@build/Release-iphoneos/SOLID-Release-1.0-dSYM.zip -F api_token='TF_TOKEN' -F team_token='TF_TEAM_TOKEN' -F notes="This is an autodeploy build from Jenkins via curl" -F notify=True 
+echo "You must export TF_TOKEN and TF_TEAM_TOKEN in Jenkins' 'Configure System' | 'Global properties' section."
+curl http://testflightapp.com/api/builds.json -F file=@build/Release-iphoneos/SOLID-Release-1.0.ipa -F dsym=@build/Release-iphoneos/SOLID-Release-1.0-dSYM.zip -F api_token='$TF_TOKEN' -F team_token='$TF_TEAM_TOKEN' -F notes="This is an autodeploy build from Jenkins via curl" -F notify=True 
 # -F distribution_lists=’<name of test distribution list>’ -F replace=False
