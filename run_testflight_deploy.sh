abaa3c3a0f814fbeaebe4e9fb2429b14c343a422
Kevin Munc <muncman@gmail.com>
2012-08-09 13:59:32 -0400

    Added distribution list to testflight script.
    Hoping I'll get the emails now…
    I'm the only one in the list, though, as well as build-maker.
    Plus there aren't binary changes every execution.

--- run_testflight_deploy.sh
+++ run_testflight_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 echo "You must setenv/export TF_TOKEN and TF_TEAM_TOKEN for this script. Maybe set in Jenkins' 'Configure System' | 'Global properties' section."
-curl http://testflightapp.com/api/builds.json -F file=@build/Release-iphoneos/SOLID-Release-1.0.ipa -F dsym=@build/Release-iphoneos/SOLID-Release-1.0-dSYM.zip -F api_token=$TF_TOKEN -F team_token=$TF_TEAM_TOKEN -F notes="This is an autodeploy build from Jenkins via curl" -F notify=True 
-# -F distribution_lists=’<name of test distribution list>’ -F replace=False
+curl http://testflightapp.com/api/builds.json -F file=@build/Release-iphoneos/SOLID-Release-1.0.ipa -F dsym=@build/Release-iphoneos/SOLID-Release-1.0-dSYM.zip -F api_token=$TF_TOKEN -F team_token=$TF_TEAM_TOKEN -F notes="This is an autodeploy build from Jenkins via curl" -F distribution_lists='memyself' -F notify=True 
+# -F replace=False
