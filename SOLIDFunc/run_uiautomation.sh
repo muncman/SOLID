f770bacfca2f6a1a94a948d594b33448938ed50f
Kevin Munc <muncman@gmail.com>
2012-08-08 20:18:07 -0400

    Updates to the UIAutomation script.
    - it now builds separately to a temp directory, rather than hardcoding or trying to otherwise obtain the build directory

--- SOLIDFunc/run_uiautomation.sh
+++ SOLIDFunc/run_uiautomation.sh
@@ -1,34 +1,42 @@
 #!/bin/sh
+# UIAutomation script. Built upon information from many others, especially Jonathan Penn and his gists.
 
 # One way to make sure the simulator isn't already running.
 osascript -e 'tell app "iPhone Simulator" to quit'
 
-# cd SOLID
-# xcodebuild -target SOLID -configuration Debug -sdk iphonesimulator TARGETED_DEVICE_FAMILY=1 clean build
-# cd ..
-
 set -e
 
-# echo "Dollar zero is"
 # echo $0
 if [ -z "$WORKSPACE" ]
 then
-	export WORKSPACE=`pwd`
+	WORKSPACE=`pwd`
 	echo "set WORKSPACE to $WORKSPACE"
 fi
 
-# TODO: app bundle env var
+INSTRUMENTS_BUILD_DIR=/tmp/SOLID_AUTOMATION
+echo INSTRUMENTS_BUILD_DIR = $INSTRUMENTS_BUILD_DIR
+mkdir -p $INSTRUMENTS_BUILD_DIR
+
+INSTRUMENTS_OUTPUT_DIR="$WORKSPACE/SOLIDFunc/output"
+echo INSTRUMENTS_OUTPUT_DIR = $INSTRUMENTS_OUTPUT_DIR
+rm -rf $INSTRUMENTS_OUTPUT_DIR
+mkdir -p $INSTRUMENTS_OUTPUT_DIR
+
+# TODO: Look into this for controlling which simulator device we get, and where we should point it.
 # /usr/libexec/PlistBuddy ./build/Debug-iphonesimulator/SOLID.app/Info.plist \
 #         -c "Delete :UIDeviceFamily" \
 #         -c "Add :UIDeviceFamily array" \
 #         -c "Add :UIDeviceFamily: integer 1"
 
-# instruments -t /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Instruments/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate "$HOME/Library/Application Support/iPhone Simulator/5.1/Applications/3C8EE392-E24F-49B7-A8AD-1E564B11DF84/SOLID.app" -e UIASCRIPT $HOME/workspace/SOLID/SOLIDFunc/testSOLID.js -e UIARESULTSPATH $HOME/workspace/SOLID/SOLIDFunc/output
-instruments -t /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Instruments/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate "$HOME/Library/Application Support/iPhone Simulator/5.1/Applications/3C8EE392-E24F-49B7-A8AD-1E564B11DF84/SOLID.app" -e UIASCRIPT "$WORKSPACE/SOLIDFunc/testSOLID.js" -e UIARESULTSPATH "$WORKSPACE/SOLIDFunc/output" 
+# cd ..
+# xcodebuild -target SOLID -configuration Debug -sdk iphonesimulator TARGETED_DEVICE_FAMILY=1 clean build
+xcodebuild -sdk iphonesimulator build CONFIGURATION_BUILD_DIR=$INSTRUMENTS_BUILD_DIR
+# cd -
 
-# run_uiautomation.rb "$HOME/Library/Application Support/iPhone Simulator/5.1/Applications/3C8EE392-E24F-49B7-A8AD-1E564B11DF84/SOLID.app" $HOME/workspace/SOLID/SOLIDFunc/testSOLID.js $HOME/workspace/SOLID/SOLIDFunc/output
-# ./run_uiautomation.rb "$HOME/Library/Application Support/iPhone Simulator/5.1/Applications/3C8EE392-E24F-49B7-A8AD-1E564B11DF84/SOLID.app" "$WORKSPACE/SOLIDFunc/testSOLID.js" "$WORKSPACE/SOLIDFunc/output"
-# "$WORKSPACE/SOLIDFunc/run_uiautomation.rb" "$HOME/Library/Application Support/iPhone Simulator/5.1/Applications/3C8EE392-E24F-49B7-A8AD-1E564B11DF84/SOLID.app" "$WORKSPACE/SOLIDFunc/testSOLID.js" "$WORKSPACE/SOLIDFunc/output"
+instruments -t /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Instruments/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate $INSTRUMENTS_BUILD_DIR/SOLID.app -e UIASCRIPT "$WORKSPACE/SOLIDFunc/testSOLID.js" -e UIARESULTSPATH $INSTRUMENTS_OUTPUT_DIR 
 
 # Another way to make sure the simulator isn't already running.
 killall -m -KILL "iPhone Simulator"
+
+# NEXT: Set up CI job to parse for "Error:" and/or "Fail:" to fail the job.
+#   OR: Use Ruby to change return value, per CocoaManifest's approach.
