3800c9356e23165d1606b844d7519665636f6519
muncman <muncman@gmail.com>
2012-10-26 04:24:29 -0400

    Updated doxygen script for brew-installed location instead of .app version

--- Doxygen/run_doxygen.sh
+++ Doxygen/run_doxygen.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 set -e
-/Applications/Doxygen.app/Contents/Resources/doxygen Doxygen/Doxyfile
+#/Applications/Doxygen.app/Contents/Resources/doxygen Doxygen/Doxyfile
+#/usr/local/bin/doxygen
+doxygen Doxygen/Doxyfile
