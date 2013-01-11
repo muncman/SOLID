e31f3c080dd9b06d776fd4f69b7e0efdef73fa7b
muncman <muncman@gmail.com>
2013-01-11 01:08:28 -0500

    Ugh. Xcode is not my friend.

--- scripts/run_gcovr.sh
+++ scripts/run_gcovr.sh
@@ -51,3 +51,11 @@ echo "Generating coverage.xml"
 # First and Second commands, from log: 
 # The full launch command: /usr/local/bin/ios-sim launch "/Users/muncman/.jenkins/jobs/code_coverage_with_gcovr/workspace/build/SOLID.app" --verbose --setenv XCInjectBundleInto="/Users/muncman/.jenkins/jobs/code_coverage_with_gcovr/workspace/build/SOLID.app/SOLID" --setenv DYLD_INSERT_LIBRARIES="/../../Library/PrivateFrameworks/IDEBundleInjection.framework/IDEBundleInjection" --setenv XCInjectBundle="/Users/muncman/.jenkins/jobs/code_coverage_with_gcovr/workspace/build/SOLIDTests.octest" --sdk 5.1 --args -SenTest All /Users/muncman/.jenkins/jobs/code_coverage_with_gcovr/workspace/build/SOLIDTests.octest
 # The full launch command: /usr/local/bin/ios-sim launch "/Users/muncman/.jenkins/jobs/code_coverage_with_gcovr/workspace/CoverageBuild/SOLID.app" --verbose --setenv XCInjectBundleInto="/Users/muncman/.jenkins/jobs/code_coverage_with_gcovr/workspace/CoverageBuild/SOLID.app/SOLID" --setenv XCInjectBundle="/Users/muncman/.jenkins/jobs/code_coverage_with_gcovr/workspace/CoverageBuild/SOLIDTests.octest" --setenv DYLD_INSERT_LIBRARIES="/../../Library/PrivateFrameworks/IDEBundleInjection.framework/IDEBundleInjection" --sdk 5.1 --args -SenTest All /Users/muncman/.jenkins/jobs/code_coverage_with_gcovr/workspace/CoverageBuild/SOLIDTests.octest
+# Formerly all in Jenkins = 
+# set -e
+# osascript -e 'tell app "iPhone Simulator" to quit'
+# INSTRUMENTS_BUILD_DIR=$WORKSPACE/build # /tmp/SOLID_AUTOMATION
+# echo INSTRUMENTS_BUILD_DIR = $INSTRUMENTS_BUILD_DIR
+# mkdir -p $INSTRUMENTS_BUILD_DIR
+# xcodebuild -target SOLIDTests -sdk iphonesimulator5.1 -arch i386 -scheme "CLI_UnitTests" CONFIGURATION_BUILD_DIR=$INSTRUMENTS_BUILD_DIR clean build TEST_AFTER_BUILD=YES
+# "$WORKSPACE/scripts/run_gcovr.sh"
