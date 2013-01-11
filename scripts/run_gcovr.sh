76dca72383d5101754b50aa59e9d867b7cbedb15
muncman <muncman@gmail.com>
2013-01-11 00:29:06 -0500

    Trying to get gcovr working with the latest Xcode.

--- scripts/run_gcovr.sh
+++ scripts/run_gcovr.sh
@@ -6,5 +6,43 @@ then
 	export WORKSPACE=`pwd`
 fi
 echo "WORKSPACE is $WORKSPACE"
+
+# TODO: killall -m -KILL "iPhone Simulator"
+
+BUILD_DIR=$WORKSPACE/CoverageBuild
+BUILD_DIR=$WORKSPACE/CoverageTemp
+
+echo .
+echo $DERIVED_FILE_DIR
+echo .
+
+# xcodebuild \
+# 	-scheme SOLIDTests \
+# 	-target SOLIDTests \
+# 	-configuration Debug \
+# 	-arch i386 \
+# 	-sdk iphonesimulator \
+# 	OBJROOT=$BUILD_DIR/build.obj \
+# 	SYMROOT=$BUILD_DIR/build.sym \
+# 	DSTROOT=$BUILD_DIR/build.dst \
+# 	SHARED_PRECOMPS_DIR=$BUILD_DIR/build.pch \
+# 	CONFIGURATION_BUILD_DIR=$BUILD_DIR \
+# 	CONFIGURATION_TEMP_DIR=$TEMP_DIR \
+# 	GCC_SYMBOLS_PRIVATE_EXTERN=NO \
+# 	COPY_PHASE_STRIP=NO \
+# 	clean build \
+# 	TEST_AFTER_BUILD=YES
+
+xcodebuild \
+	-target SOLIDTests \
+	-configuration Debug \
+	-arch i386 \
+	-sdk iphonesimulator \
+	CONFIGURATION_BUILD_DIR=$BUILD_DIR \
+	CONFIGURATION_TEMP_DIR=$TEMP_DIR \
+	TEST_AFTER_BUILD=YES \
+	clean build
+
 echo "Generating coverage.xml"
-"$WORKSPACE/gcovr" -r "$WORKSPACE" --e '.*UnitTests.*' --xml > "$WORKSPACE/coverage.xml"
+# "$WORKSPACE/gcovr" -r "$WORKSPACE" --e '.*UnitTests.*' --xml > "$WORKSPACE/coverage.xml"
+"$WORKSPACE/gcovr" -r "$WORKSPACE" --e '.*UnitTests.*' --keep --xml > "$WORKSPACE/coverage.xml"
